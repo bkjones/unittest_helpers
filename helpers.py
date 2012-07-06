3fe4a008882ed310bf8317dabecacfd0f8327748
Brian K. Jones <bkjones@gmail.com>
2012-07-05 22:54:32 -0400

    Initial commit.

---
+++ helpers.py
@@ -0,0 +1,30 @@
+class KeyLookupCountError(Exception):
+    """Raised when a key in MockDict is looked up some number of times
+    different from how many are expected/asserted.
+
+    """
+
+class MockDict(dict):
+    def __init__(self, *args, **kwargs):
+        self.key_lookups = list()
+        self.updates = list()
+        super(MockDict, self).__init__(*args, **kwargs)
+
+
+    def __getitem__(self, item):
+        self.key_lookups.append(item)
+        return super(MockDict, self).__getitem__(item)
+
+    def __setitem__(self, item, value):
+        self.updates.append((item, value))
+        if isinstance(value, dict):
+            value = MockDict(value)
+        return super(MockDict, self).__setitem__(item, value)
+
+    def assert_key_looked_up_once(self, key):
+        actual_lookup_count = self.key_lookups.count(key)
+        try:
+            assert actual_lookup_count == 1
+        except AssertionError:
+            raise KeyLookupCountError("Expected %s to be looked up once, but was looked up %s times" % (key, actual_lookup_count))
+
