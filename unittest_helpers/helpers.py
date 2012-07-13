8a7ec9553a75228f1f0fabd91b02c02285d1df90
Dan Tracy <dant@aweber.com>
2012-07-13 16:06:29 -0400

    Made remove_decorator a context manager

--- unittest_helpers/helpers.py
+++ unittest_helpers/helpers.py
@@ -2,72 +2,36 @@ from contextlib2 import contextmanager, ExitStack
 from mock import patch
 
 
-def remove_decorator(decorated_func, remove_all_decorators=False):
+@contextmanager
+def remove_decorator(func, remove_all_decorators=False):
     """ Remove the last applied decorator from the function passed and return
     the original, undecorated function.  If ``remove_all_decorators`` is set
     to True then every decorator that has been applied to the function will
     be removed, including timers and context managers.
 
+        :note: This does not modify the passed function, it simply retrieves
+            the original function from within the decorated one.
+
         :returns: Function without the last applied decorator
     """
 
-    # If the function object doesn't have a closure object then it hasnt been
-    # decorated, so just ignore it.
-    if not hasattr(decorated_func, '__closure__') or not decorated_func.__closure__:
-        return decorated_func
+    # If the function object has a closure object then try to recover
+    # undecorated function
+    if hasattr(func, '__closure__') and func.__closure__:
+        # The cell contents of the closure is the original functions
+        closure = func.__closure__[0]
+        undecorated_func = closure.cell_contents
 
-    # The cell contents of the closure is the original functions
-    closure = decorated_func.__closure__[0]
-    original_func = closure.cell_contents
+        while remove_all_decorators and hasattr(undecorated_func, '__closure__'):
+            closure = func.__closure__[0]
+            undecorated_func = closure.cell_contents
 
-    if hasattr(decorated_func, '_previously_decorated_function'):
-        # If we are recursing then we need to retain the original function
-        # and not overwrite it each time we recurse
-        original_func._previously_decorated_function = \
-                         decorated_func._previously_decorated_function
+        # Return the undecorated function to the with statement block
+        # After the __exit__ function is called the originally decorated
+        # function will be in place once again
+        yield undecorated_func
     else:
-        original_func._previously_decorated_function = decorated_func
-
-    if remove_all_decorators and hasattr(original_func, '__closure__'):
-        return remove_decorator(original_func, remove_all_decorators)
-    else:
-        return original_func
-
-
-def restore_decorator(undecorated_func):
-    """ Restores the previously removed decorator
-
-        :returns: The original function with its decorators intact
-
-        >>> @contextmanager
-        ....    def foo():
-        ........    pass
-
-        >>> @contextmanager
-        ... @contextmanager
-        ... def bar():
-        ....... pass
-
-        >>> not_foo = remove_decorator(foo)
-
-        >>> assert not_foo != foo
-        >>> assert restore_decorator(not_foo) == foo
-
-        >>> one_removed = remove_decorator(bar)
-        >>> all_removed = remove_decorator(bar, True)
-
-        >>> assert one_removed != all_removed != bar
-        >>> assert restore_decorator(one_removed) == \
-                   restore_decorator(all_removed) == \
-                   bar
-
-
-    """
-    if not hasattr(undecorated_func, '_previously_decorated_function'):
-        # We didnt remove any decorators on this function
-        return undecorated_func
-    else:
-        return undecorated_func._previously_decorated_function
+        yield func
 
 
 @contextmanager
