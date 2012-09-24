af1b640ff9bf3dc737aa82563f5ebce46b845130
Brian K. Jones <bkjones@gmail.com>
2012-09-24 16:20:11 -0400

    Use ExitStack as a contextmanager inside patches

--- unittest_helpers/helpers.py
+++ unittest_helpers/helpers.py
@@ -18,11 +18,10 @@ def patches(*args):
         self.patched.mycls.some_method.return_value = True
 
     """
-    stack = ExitStack()
-    for thing in args:
-        name = thing.split('.')[-1]
-        new_patch = patch(thing)
-        triggered_patch = stack.enter_context(new_patch)
-        setattr(stack, name, triggered_patch)
-
-    yield stack
+    with ExitStack() as stack:
+        for thing in args:
+            name = thing.split('.')[-1]
+            new_patch = patch(thing)
+            triggered_patch = stack.enter_context(new_patch)
+            setattr(stack, name, triggered_patch)
+        yield stack
